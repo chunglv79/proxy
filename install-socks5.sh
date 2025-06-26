ddb2aa581f3973640e757374489e61c9223dd4bf
chunglv79 <laivuchungdlk87@gmail.com>
2025-06-27 01:05:49 +0700

    Update install-socks5.sh

--- install-socks5.sh
+++ install-socks5.sh
@@ -55,7 +55,7 @@ EOF
 
 # Tạo user
 useradd -M -s /usr/sbin/nologin mrmeo
-echo "mrmeo:pmbhgqzajsdfsd@dfgd87*&^%" | chpasswd
+echo "mrmeo:pmbhgqzajsdfsddfgd87*&^%" | chpasswd
 #set quyen file log
 sudo touch /var/log/danted.log
 sudo chmod 666 /var/log/danted.log
